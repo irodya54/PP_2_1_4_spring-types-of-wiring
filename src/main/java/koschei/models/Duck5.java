5c5fe7de1ecbc2fd0a7415e275ccc11bdbc134cb
Radion Merzkyakov <ya.irodya54@yandex.ru>
2022-07-30 15:03:24 +0300

    very vvery final

--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -3,9 +3,9 @@ package koschei.models;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class Duck5 {
-    @Autowired
+//    @Autowired
     private Egg6 egg;
 
     public Duck5(Egg6 egg) {
